d570f4ebc97bba700b2a19fa922192939fe91934
Aleksandar1105 <todoroski.aleksandar@gmail.com>
2023-03-02 14:19:53 +0100

    Solved Task 1 of Homework#2

---
+++ HOMEWORK#2/Task1_RealCalculator/Program.cs
@@ -0,0 +1,34 @@
+﻿
+using System.Data.Common;
+
+Console.WriteLine("Please enter your first number:");
+bool userInputNumber1 = int.TryParse(Console.ReadLine(), out int number1);
+
+Console.WriteLine("Please enter your second number:");
+bool userInputNumber2 = int.TryParse(Console.ReadLine(), out int number2);
+
+Console.WriteLine("Please choose operation:");
+bool chooseOperator = char.TryParse(Console.ReadLine(), out char chosenOperator);
+
+
+if (chosenOperator == '+'){
+    int result = number1 + number2;
+    Console.WriteLine("The result is: " + result);
+}
+else if (chosenOperator == '-'){
+    int result = number1 - number2;
+    Console.WriteLine("The result is: " + result);
+}
+else if (chosenOperator == '*')
+{
+    int result = number1 *number2;
+    Console.WriteLine("The result is: " + result);
+}
+else if (chosenOperator == '/')
+{
+    int result = number1 / number2;
+    Console.WriteLine("The result is: " + result);
+}
+
+
+Console.ReadLine();
