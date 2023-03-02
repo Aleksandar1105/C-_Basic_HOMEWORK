922ad78b7eac482c5acf41c5610d1250e4933504
Aleksandar1105 <todoroski.aleksandar@gmail.com>
2023-03-02 14:36:43 +0100

    Solved Task 2 of Homework#2

--- HOMEWORK#2/Task1_RealCalculator/Program.cs
+++ HOMEWORK#2/Task1_RealCalculator/Program.cs
@@ -1,13 +1,11 @@
 ﻿
-using System.Data.Common;
-
-Console.WriteLine("Please enter your first number:");
+Console.WriteLine("Enter the First number:");
 bool userInputNumber1 = int.TryParse(Console.ReadLine(), out int number1);
 
-Console.WriteLine("Please enter your second number:");
+Console.WriteLine("Enter the Second number:");
 bool userInputNumber2 = int.TryParse(Console.ReadLine(), out int number2);
 
-Console.WriteLine("Please choose operation:");
+Console.WriteLine("Enter the Operation:");
 bool chooseOperator = char.TryParse(Console.ReadLine(), out char chosenOperator);
 
 
@@ -28,6 +26,10 @@ else if (chosenOperator == '/')
 {
     int result = number1 / number2;
     Console.WriteLine("The result is: " + result);
+}
+else
+{
+    Console.WriteLine("You entered invalid operator. Please try againg with: +, - , * or / ");
 }
 
 
---
+++ HOMEWORK#2/Task2_AverageNumber/Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+Console.WriteLine("Enter the First number:");
+bool userInputNumber1 = int.TryParse(Console.ReadLine(), out int number1);
+//Console.WriteLine(userInputNumber1);
+
+Console.WriteLine("Enter the Second number:");
+bool userInputNumber2 = int.TryParse(Console.ReadLine(), out int number2);
+
+Console.WriteLine("Enter the Third number:");
+bool userInputNumber3 = int.TryParse(Console.ReadLine(), out int number3);
+
+Console.WriteLine("Enter the Fourth number:");
+bool userInputNumber4 = int.TryParse(Console.ReadLine(), out int number4);
+
+int averageNumber = (number1 + number2 + number3 + number4) / 4;
+
+Console.WriteLine("The average of " + number1 + ", " + number2 + ", " + number3 + " and " + number4 + " is: " + averageNumber);
+
+
+
+Console.ReadLine(); 
