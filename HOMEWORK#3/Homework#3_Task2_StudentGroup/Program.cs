b73f16c0e3f5f6bb590ea225702eb7612324149e
Aleksandar1105 <todoroski.aleksandar@gmail.com>
2023-03-04 22:28:33 +0100

    added

---
+++ HOMEWORK#3/Homework#3_Task2_StudentGroup/Program.cs
@@ -0,0 +1,33 @@
+﻿
+string[] studentsG1 = new string[5] { "Aleksandar", "Dragica", "Lina", "Natasa", "Kristijan" };
+string[] studentsG2 = new string[5] { "Ilija", "Dragan", "Marko", "Dejan", "Matej" };
+
+Console.WriteLine("Enter student group:");
+bool isGroupNumberInput = int.TryParse(Console.ReadLine(), out int groupNumber);
+
+if (!isGroupNumberInput)
+{
+    Console.WriteLine("You entered invalid input!");
+}
+else
+{
+    if (groupNumber == 1)
+    {
+        for (int i = 0; i < studentsG1.Length; i++)
+        {
+            Console.WriteLine(studentsG1[i]);
+        }
+    }
+    else if (groupNumber == 2)
+    {
+        for (int i = 0; i < studentsG2.Length; i++)
+        {
+            Console.WriteLine(studentsG2[i]);
+        }
+    }
+    else
+    {
+        Console.WriteLine("Enter 1 for students G1, or 2 for students G2");
+    }
+}
+
